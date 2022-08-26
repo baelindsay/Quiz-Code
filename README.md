4c1de82021552027943da268bfb43ce496320f26
baelindsay <109304038+baelindsay@users.noreply.github.com>
2022-08-25 22:33:21 -0500

    add readMe File

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Quiz-Code
+Quiz about JavaScript fundamentals with timer & score-board
+
+## Description
+This project is a multiple choice timed quiz. The user has 60 seconds to answer 3 questions. Every correct answer will log 10 points. However, for every incorrect answer the timer will subtract 10 seconds. When the time runs out or when all the questions are answered, the option to enter the users nickname will appear and if the user submits their score then their score will be stored locally to a high-score board. 
+
+### Results
+    * With this project I brutally learned I have a lot more to study.
+
+    * I implemented local storage for the first time.
--- script.js
+++ script.js
@@ -1,14 +1,117 @@
-const startingMinutes = 10;
-let time = startingMinutes * 60;
+const quizContainer = document.querySelector('#question');
+const answerA =  document.getElementById('answer-A');
+const answerB =  document.getElementById('answer-B');
+const answerC =  document.getElementById('answer-C');
+const answerD =  document.getElementById('answer-D');
+const finalScoreEl = document.getElementById('fin-score');
+const nicknameEl = document.getElementById('nickname');
+const highScoreListEl = document.getElementById('highscore-list');
+const timerEl = document.getElementById('timer');
 
-const countdownEl = document.getElementById("countdown");
+let secondsLeft = 60;
+let currentScore = 0;
+let currentQ = -1;
+let finalScore;
 
-setInterval(updateCountdown, 1000);
+// Move from current to next div
+function changeDiv(curr, next) {
+    document.getElementById(curr).classList.add('hidden');
+    document.getElementById(next).removeAttribute('class');
+};
 
-function updateCountdown() {
-    const minutes = Math.floor(time / 60);
-    let seconds = time % 60;
+// Start Button Click
+function startGame() {
+    changeDiv('home', 'question-container');
+    nextQuestion();
+    startTimer();
+};
 
-    countdownEl.innerHTML = '${minutes}: ${seconds}';
-    time--;
-}ß
+startButton.addEventListener('click', start)
+
+// Timer function
+function startTimer() {
+    timerEl.textContent = secondsLeft;
+    let timerInterval  = setInterval(
+        () => {
+            secondsLeft--;
+            timerEl.textContent = secondsLeft;
+            if (secondsLeft <= 0) {
+                clearInterval(timerInterval);
+                endQuiz();
+            }
+        }
+    )
+};
+
+// Question Function 
+function nextQuestion() {
+    currentQuestion++;
+
+    if (currentQuestion == shuffledQuestions.length) {
+        secondsLeft = 0;
+        endQuiz();
+    } else {
+        questionEl.textContent = shuffleQuestions[currentQuestion].question;
+        let arr = [answerA, answerB, answerC, answerD];
+        let i =0;
+        arr.forEach(element => {
+            element.textContent = shuffleQuestions[currentQuestion].answersArray[i].answer;
+            i++
+        }, i);
+    };
+};
+
+// When human clicks answer button
+function handleAnswerClick(event) {
+    // this gets the correct answer sting
+    let rightAnswer = getRightAnswer(currentQ);
+    //  t
+    if (event.target.textContent === rightAnswer) {
+        currentScore += 10;
+    } else {
+        //this subtracts time when human clicks wrong answer
+        secondsLeft -= 10;
+    }
+};
+
+function getRightAnswer (currentQuestion) {
+    let arr = suffleQuestions[currentQuestion].answerArray;
+    // this is a loop that goes through answerArray 
+    for (let x = 0; x < arr.length; x++) {
+        if (arr[x].right) {
+            // this returns the right answer
+            return arr[x].answer
+        }
+    }
+};
+
+
+//Array that holds all my questions
+const questionsBank = [
+    {
+        question: 'What is an array in JavaScript?',
+        answerArray: [
+            {answer: 'the thing in bows', correct: false },
+            {answer: 'A function for element', correct: false},
+            {answer: 'A Boolean', correct: false},
+            {answer: 'an ordered list of value', correct: true}]
+    },
+
+    {
+        question: 'Which one is the correct HTML element to define emphasized text?',
+        answers: [
+            {answer: '<i></i>', correct: false },
+            {answer: '<em></emt', correct: true},
+            {answer: '<bold></bold>', correct: false},
+            {answer: '<ital></ital>', correct: false}]
+    },   
+
+    {
+        question: "How does a 'for' loop start?",
+        answers: [
+            {answer: 'for (i=0; i <= 5)', correct: false },
+            {answer: 'for(i = 0; i <=5; i++)', correct: false},
+            {answer: 'for i = 1 to 5', correct: false},
+            {answer: '(i <=5; i++)', correct: true}]
+    }
+];
